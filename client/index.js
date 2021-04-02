53e8c04c7c1634e8061a5f1e552dc7172ecd60ac
sirmcclay <mcclay.design@gmail.com>
2021-04-02 18:22:47 -0300

    fix for cookies handle but this is nor needed anymore because of Apollo 2.0

--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, IndexRoute, Route } from 'react-router';
 
 import App from './components/App';
 
+// THIS FIX IS NOT NEEDED ANYMORE - Because Apollo 2.0
+const networkInterface = createNetworkInterface({
+	uri: '/graphql',
+	opts: {
+		credentials: 'same-origin',
+	},
+});
+
 const client = new ApolloClient({
+	networkInterface, // FIX NOT NEEDED ANYMORE
 	dataIdFromObject: (o) => o.id,
 });
 
