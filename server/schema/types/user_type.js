68c5cca6b05f2a583d0a13edbffa5d34b40e8bbf
sirmcclay <mcclay.design@gmail.com>
2021-04-02 16:32:40 -0300

    add id as graphqlid type to graphql succeed to track id and objects nested one with each other

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,9 +1,11 @@
+const { GraphQLID } = require('graphql');
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString } = graphql;
 
 const UserType = new GraphQLObjectType({
 	name: 'UserType',
 	fields: {
+		id: { type: GraphQLID },
 		email: { type: GraphQLString },
 	},
 });
