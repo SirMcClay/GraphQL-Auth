432570f91baa0d0feb6f758db03dcd288df27c3a
sirmcclay <mcclay.design@gmail.com>
2021-04-03 15:32:38 -0300

    form structure

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class AuthForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { email: '', password: '' };
+	}
+
 	render() {
-		return <form></form>;
+		return (
+			<div className="row">
+				<form className="col s4">
+					<div className="input-field">
+						<label>Email</label>
+						<input />
+					</div>
+					<div className="input-field">
+						<label>Password</label>
+						<input />
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
