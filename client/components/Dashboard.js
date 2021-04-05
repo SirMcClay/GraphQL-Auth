d61f85b8fdab8395b407464a9607ba93711b8162
sirmcclay <mcclay.design@gmail.com>
2021-04-05 11:43:32 -0300

    dashboard component and route

---
+++ client/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+	return (
+		<div>
+			<h3>DashBoard</h3>
+			<div>You are logged in.</div>
+		</div>
+	);
+};
+
+export default Dashboard;
--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import { Router, hashHistory, IndexRoute, Route } from 'react-router';
 import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
 
 // THIS FIX IS NOT NEEDED ANYMORE - Because Apollo 2.0
 const networkInterface = createNetworkInterface({
@@ -28,6 +29,7 @@ const Root = () => {
 				<Route path="/" component={App}>
 					<Route path="login" component={LoginForm} />
 					<Route path="signup" component={SignupForm} />
+					<Route path="dashboard" component={Dashboard} />
 				</Route>
 			</Router>
 		</ApolloProvider>
